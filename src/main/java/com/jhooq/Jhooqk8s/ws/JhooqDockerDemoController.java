2a73c90b9f13fa14658895a3b9d6e10d09368e3f
MEsSayed <m.e.ismael@gmail.com>
2021-08-09 20:42:51 +0200

    Add files via upload

---
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -0,0 +1,13 @@
+package com.jhooq.Jhooqk8s.ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JhooqDockerDemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello - My name is Muhammad Es-Sayed (Udacity Devops capstone)";
+    }
+}
